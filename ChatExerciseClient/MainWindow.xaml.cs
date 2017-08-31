b608934c80fbf7f053fc8db2dccf02259e4a2e19
Kasper Duch Jensen <kasp3939@edu.eal.dk>
2017-08-31 14:25:04 +0200

    Add project files.

---
+++ ChatExerciseClient/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ChatExerciseClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SendMessage(object sender, RoutedEventArgs e) //send messages
+        {
+
+        }
+
+        public void MessageItself(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        public void Client()
+        {
+            TcpClient client = new TcpClient("localhost", 11000);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+            StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true } ;
+      
+            string messageToSend = Console.ReadLine();
+            
+        }
+
+       
+    }
+}
